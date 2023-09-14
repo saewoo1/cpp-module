fc533bc0fc54f97a960aaad8bac2f7bbd50e02a5
Sohyun Park <sohyupar@cx2r2s2.42seoul.kr>
2023-09-14 19:53:23 +0900

    add

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,6 +1,7 @@
 #include "BitcoinExchange.hpp"
 #include <fstream>
 #include <string>
+#include <sstream>
 
 void BitcoinExchange::checkCsvFile() {
     std::ifstream csv("data.csv");
@@ -38,7 +39,29 @@ void BitcoinExchange::checkCsvFile() {
 //split으로 타입 검수, 윤년 검수
 bool BitcoinExchange::validateDate(std::string date)
 {
-    return false;
+    if (date.length() != 10) return false;
+    std::string splitted;
+    std::stringstream ss(date);
+
+    int year, month, day;
+    int i = 0;
+
+    while (std::getline(ss, splitted, '-')) {
+        if (i == 0) {
+            std::istringstream(splitted) >> year;
+            if (year < 1000 || year > 9999) return false;
+        } else if (i == 1) {
+            std::istringstream(splitted) >> month;
+            if (month < 1 || month > 12) return false;
+        } else if (i == 2) {
+            std::istringstream(splitted) >> day;
+            //윤달 계산.. 어케하노
+        }
+        
+    }
+    if (i != 3) return false;
+
+    return true;
 }
 
 //범위 검수?
