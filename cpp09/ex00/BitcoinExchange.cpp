87d4c85c5cd0cf67b2ead43cee396efff91d1251
Sohyun Park <sohyupar@cx2r2s2.42seoul.kr>
2023-09-14 20:41:12 +0900

    add

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 
+//csv 파일을 읽은 후 map에 담아주기
 void BitcoinExchange::checkCsvFile() {
     std::ifstream csv("data.csv");
     std::string read;
@@ -14,29 +15,37 @@ void BitcoinExchange::checkCsvFile() {
         std::cout << "Error: invalid database file" << std::endl;
         throw Error();
     }
-
     if (std::getline(csv, read).eof()) {
         std::cout << "Error: empty database file" << std::endl;
         throw Error();
     }
-    while (std::getline(csv, read)) {
-        if (read != "date,exchange_rate") {
-            comma_idx = read.find(',');
-            if (!validateDate(read.substr(0, comma_idx))) {
-                std::cout << "Error: invalid date" << std::endl;
-                throw Error();
-            }
 
-            if (!validateValue(read.substr(comma_idx + 1, read.length()))) {
-                std::cout << "Error: invalid value" << std::endl;
-                throw Error();
-            }
-        }
+    //첫 줄 읽기
+    std::getline(csv, read);
+    if (read != "date,exchange_rate") {
+
+        std::cout << "Error: invalid first Line" << std::endl;
+        throw Error();
     }
-    //데이터 어케 담지??
+
+    while (std::getline(csv, read)) {
+        comma_idx = read.find(',');
+        if (!validateDate(read.substr(0, comma_idx))) {
+            std::cout << "Error: invalid date" << std::endl;
+            throw Error();
+        }
+
+        if (!validateValue(read.substr(comma_idx + 1, read.length()))) {
+            std::cout << "Error: invalid value" << std::endl;
+            throw Error();
+        }
+        std::istringstream(read.substr(comma_idx + 1, read.length())) >> value;
+        bitcoinData[read.substr(0, comma_idx)] = value;
+    }
 }
 
 //split으로 타입 검수, 윤년 검수
+// 짝수 월일 땐 6월 전까진 30일까지, 홀수 월일땐 7월 후부터 30일까지
 bool BitcoinExchange::validateDate(std::string date)
 {
     if (date.length() != 10) return false;
@@ -56,17 +65,24 @@ bool BitcoinExchange::validateDate(std::string date)
         } else if (i == 2) {
             std::istringstream(splitted) >> day;
             if (day < 1 || day > 31) return false;
-            //윤달 계산.. 어케하노
+            if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11)) return false;
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+               if (month == 2 && day > 29) return false;
+            } else if (month == 2 && day > 28) return false;
         }
-        
+        i++;
     }
     if (i != 3) return false;
-
     return true;
 }
 
 //범위 검수?
 bool BitcoinExchange::validateValue(std::string value)
 {
-    return false;
+    char *ptr = NULL;
+    double val = std::strtod(value.c_str(), &ptr);
+    if (val == 0.0 && !std::isdigit(value[0])) return false;
+    if (*ptr && std::strcmp(ptr, "f")) return false;
+    if (val < 0 || val > 1000) return false;
+    return true;
 }
